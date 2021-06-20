1c14f221d093defe6133c6026dcb0e968a70d071
poseidon-code <pritamhalder05062000@gmail.com>
2021-06-20 10:01:41 +0530

    add: routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { useAuth } from './contexts/AuthContext';
 import api from './axios';
+import PrivateRoute from './PrivateRoute';
 
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
 import Volunteer from './pages/Volunteer';
+import Ngo from './pages/Ngo';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App = () => {
+    const { user } = useAuth();
+
     useEffect(() => {
         api.get('/').then((res) => console.log(res.data));
     }, []);
@@ -16,7 +23,9 @@ const App = () => {
             <Layout>
                 <Switch>
                     <Route path='/' exact component={Home} />
-                    <Route path='/volunteer/:id' component={Volunteer} />
+                    {!user.isAuthorised && <Route path='/register' exact component={Register} />}
+                    {!user.isAuthorised && <Route path='/login' exact component={Login} />}
+                    <PrivateRoute path='/dashboard' component={user.isVolunteer ? Volunteer : Ngo} />
                     <Route path='*' exact>
                         <Redirect to='/' />
                     </Route>
