b30f3882edafb3bc41c1e1772aa9f81d8b2cb1a5
poseidon-code <pritamhalder05062000@gmail.com>
2021-06-20 13:45:45 +0530

    update: ambulance posting

--- client/src/components/Volunteer/Post/Facilities/Ambulance.jsx
+++ client/src/components/Volunteer/Post/Facilities/Ambulance.jsx
@@ -1,5 +1,13 @@
 const Ambulance = (props) => {
-    return <h2>{JSON.stringify(props.post)}</h2>;
+    return (
+        <>
+            <h2>Type: {props.post.type}</h2>
+            <h3>Service Provider : {props.post.info.serviceProvider}</h3>
+            <h3>Contact : {props.post.info.contactNum}</h3>
+            <h4>Location : {props.post.info.location}</h4>
+            <h4>Votes : {props.post.votes}</h4>
+        </>
+    );
 };
 
 export default Ambulance;
