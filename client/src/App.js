b97d396837180860912f2f54e00d647d4e378824
poseidon-code <pritamhalder05062000@gmail.com>
2021-06-27 20:08:48 +0530

    prettified

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { useAuth } from './contexts/AuthContext';
 import api from './axios';
 import PrivateRoute from './PrivateRoute';
-import Donate from "./components/Facility/Donate/Donate"
+import Donate from './components/Facility/Donate/Donate';
 import Layout from './components/Layout/Layout';
 import Home from './pages/Home';
 import Volunteer from './pages/Volunteer';
@@ -14,7 +14,6 @@ import Register from './pages/Register';
 import DetailDonate from './components/Facility/Donate/DetailDonate';
 import Recruitments from './pages/Recruitments';
 
-
 const App = () => {
     const { user } = useAuth();
 
@@ -22,45 +21,42 @@ const App = () => {
         api.get('/').then((res) => console.log(res.data));
     }, []);
 
-
-
-    const [ funds, setFunds ] = useState([]);
+    const [funds, setFunds] = useState([]);
 
     const getFundRaise = () => {
-        api.get('/ngo/getFundraise')
-        .then((res)=>{
+        api.get('/ngo/getFundraise').then((res) => {
             setFunds(res.data);
-        })
-    }
+        });
+    };
+
     useEffect(() => {
-        console.log("Hello");
         getFundRaise();
-    }, [])
+    }, []);
 
     return (
-        <div>
-            <Layout>
-                <Switch>
-                    <Route path='/' exact component={Home} />
-                    {!user.isAuthorised && <Route path='/register' exact component={Register} />}
-                    {!user.isAuthorised && <Route path='/login' exact component={Login} />}
-                    <PrivateRoute path='/dashboard' component={user.isVolunteer ? Volunteer : Ngo} />
-                    <Route path='/facility/donate'><Donate funds={funds} /></Route>
-                    <PrivateRoute path='/recruitments' component={Recruitments} />
-                    <Route path='/facility/:type' component={Facility} />
-                    <Route path='/fund/donate/:id' component={DetailDonate} />
-                    {user.isAuthorised ? (
-                        <Route path={['/login', '/register']} exact>
-                            <Redirect to='/dashboard' />
-                        </Route>
-                    ) : (
-                        <Route path='*' exact>
-                            <Redirect to='/' />
-                        </Route>
-                    )}
-                </Switch>
-            </Layout>
-        </div>
+        <Layout>
+            <Switch>
+                <Route path='/' exact component={Home} />
+                {!user.isAuthorised && <Route path='/register' exact component={Register} />}
+                {!user.isAuthorised && <Route path='/login' exact component={Login} />}
+                <PrivateRoute path='/dashboard' component={user.isVolunteer ? Volunteer : Ngo} />
+                <Route path='/facility/donate'>
+                    <Donate funds={funds} />
+                </Route>
+                <PrivateRoute path='/recruitments' component={Recruitments} />
+                <Route path='/facility/:type' component={Facility} />
+                <Route path='/fund/donate/:id' component={DetailDonate} />
+                {user.isAuthorised ? (
+                    <Route path={['/login', '/register']} exact>
+                        <Redirect to='/dashboard' />
+                    </Route>
+                ) : (
+                    <Route path='*' exact>
+                        <Redirect to='/' />
+                    </Route>
+                )}
+            </Switch>
+        </Layout>
     );
 };
 
